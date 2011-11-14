686327c2bd171a71504ef8994ad84302842d6023
jrowlandls <unknown>
2011-11-14 16:13:34 +0000

    doxygen usage example

---
+++ mainPage/doxygen/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Hello World main Function
+ * @param argc integer argument count
+ * @param argv array of string arguments
+ * @return integer error code
+ */
+int main(int argc, char * argv[])
+{
+    printf("hello world\n");
+    return 0;
+}
