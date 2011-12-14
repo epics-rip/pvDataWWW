606af5825e9fd98a1fc35144c5380afb23ae1a55
jrowlandls <unknown>
2011-12-14 16:52:30 +0000

    doxgen build and push script

--- scripts/syncdoxygen.sh
+++ scripts/syncdoxygen.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-PROJ="$1"
-TD=$(mktemp -d)
-hg clone ssh://jrowlandls@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/$PROJ $TD
-(cd $TD && doxygen)
-rsync -avz $TD/documentation/html jrowlandls,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/doxygen/$PROJ
+USER=jrowlandls
+PVDATATAG=tip
+PVACCESSTAG=tip
+PVIOCTAG=tip
+hg clone -r $PVDATATAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataCPP
+hg clone -r $PVACCESSTAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvAccessCPP
+hg clone -r $PVIOCTAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvIOCCPP
+cp RELEASE.local CONFIG_SITE.local pvDataCPP/configure
+cp RELEASE.local CONFIG_SITE.local pvAccessCPP/configure
+cp RELEASE.local CONFIG_SITE.local pvIOCCPP/configure
+# don't build the example that depends on Normative Types
+patch -p0 < pvIOCCPP_noNT.patch
+(cd pvDataCPP && make && doxygen)
+(cd pvAccessCPP && make && doxygen)
+(cd pvIOCCPP && make && doxygen)
+rsync -avz pvDataCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvDataCPP/$PVDATATAG
+rsync -avz pvAccessCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvAccessCPP/$PVACCESSTAG
+rsync -avz pvIOCCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvIOCCPP/$PVIOCTAG
