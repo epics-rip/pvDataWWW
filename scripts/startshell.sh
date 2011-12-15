795b03a65f380567ef5f56ed77467a4e78a1b205
jrowlandls <unknown>
2011-12-15 12:01:07 +0000

    updated javadoc release script and stubs

---
+++ scripts/startshell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# so I don't forget
+ssh -t jrowlandls,epics-pvdata@shell.sourceforge.net create
--- scripts/syncjavadoc.sh
+++ scripts/syncjavadoc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-PROJ="$1"
-TD=$(mktemp -d)
-hg clone ssh://jrowlandls@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/$PROJ $TD
-(cd $TD && javadoc -d javadoc -sourcepath src -subpackages org.epics)
-rsync -avz $TD/javadoc jrowlandls,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/doxygen/$PROJ
+USER=jrowlandls
+TAG=tip
+hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataJava
+hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvAccessJava
+hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvIOCJava
+(cd pvDataJava && javadoc -d documentation/html -sourcepath src -subpackages org.epics)
+(cd pvAccessJava && javadoc -d documentation/html -sourcepath src -subpackages org.epics)
+(cd pvIOCJava && javadoc -d documentation/html -sourcepath src -subpackages org.epics)
+rsync -avz pvDataJava/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvDataJava/$TAG
+rsync -avz pvAccessJava/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvAccessJava/$TAG
+rsync -avz pvIOCJava/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvIOCJava/$TAG
