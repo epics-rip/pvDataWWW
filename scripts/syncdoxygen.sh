54862017e9d20cb071901d684991e7f064bf2a78
jrowlandls <unknown>
2011-12-15 12:56:50 +0000

    added release docs

--- scripts/syncdoxygen.sh
+++ scripts/syncdoxygen.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 USER=jrowlandls
-PVDATATAG=test1
-PVACCESSTAG=test1
-PVIOCTAG=test1
-hg clone -r $PVDATATAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataCPP
-hg clone -r $PVACCESSTAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvAccessCPP
-hg clone -r $PVIOCTAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvIOCCPP
+TAG=test1
+# better use functions or loops in BASH!
+hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataCPP
+hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvAccessCPP
+hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvIOCCPP
 cp RELEASE.local CONFIG_SITE.local pvDataCPP/configure
 cp RELEASE.local CONFIG_SITE.local pvAccessCPP/configure
 cp RELEASE.local CONFIG_SITE.local pvIOCCPP/configure
@@ -14,6 +13,6 @@ patch -p0 < pvIOCCPP_noNT.patch
 (cd pvDataCPP && make && doxygen)
 (cd pvAccessCPP && make && doxygen)
 (cd pvIOCCPP && make && doxygen)
-rsync -avz pvDataCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvDataCPP/$PVDATATAG
-rsync -avz pvAccessCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvAccessCPP/$PVACCESSTAG
-rsync -avz pvIOCCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvIOCCPP/$PVIOCTAG
+rsync -avz pvDataCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvDataCPP/$TAG
+rsync -avz pvAccessCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvAccessCPP/$TAG
+rsync -avz pvIOCCPP/documentation $USER,epics-pvdata@web.sourceforge.net:/home/project-web/epics-pvdata/htdocs/docbuild/pvIOCCPP/$TAG
--- scripts/tagtip.sh
+++ scripts/tagtip.sh
@@ -7,7 +7,3 @@ hg clone ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvIOCCP
 (cd pvDataCPP && hg tag $TAG -u $USER && hg commit -m 'tagged by script' -u $USER && hg push)
 (cd pvAccessCPP && hg tag $TAG -u $USER && hg commit -m 'tagged by script' -u $USER && hg push)
 (cd pvIOCCPP && hg tag $TAG -u $USER && hg commit -m 'tagged by script' -u $USER && hg push)
-
-
-
-
