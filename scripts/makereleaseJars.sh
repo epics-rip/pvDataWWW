eb3f54f8fcea44aac238dffae43ff35004a925b6
Unknown <greg@slac.stanford.edu>
2013-01-11 17:34:00 +0100

    For beta 2

--- scripts/makereleaseJars.sh
+++ scripts/makereleaseJars.sh
@@ -23,16 +23,19 @@
 #
 # ----------------------------------------------------------------------------
 # Auth: 20-Dec-2011, Greg White (greg@slac.stanford.edu) 
-# Mod:  09-Jan-2011, Greg White (greg@slac.stanford.edu) 
-#       Added creating a tar.gz, converted upload of exampleJava.jar to only 
-#       including its source in the tar.gz (more useful than the jar for examples)
-#       and also include the common dir in the tar.gz. 
+# Mod:  11-Jan-2013, Greg White (greg@slac.stanford.edu)
+#       Updated TAG so as to build first beta 2 release. Also, removed references to pvService.
 #       07-Feb-2012, Greg White (greg@slac.stanford.edu)
 #       Added bundling pvService, since that's no again required after stuff 
 #       removed from pvData.
+#       09-Jan-2011, Greg White (greg@slac.stanford.edu) 
+#       Added creating a tar.gz, converted upload of exampleJava.jar to only 
+#       including its source in the tar.gz (more useful than the jar for examples)
+#       and also include the common dir in the tar.gz. 
+
 # ============================================================================
 USER=gregorywhite
-TAG=1.0.1-BETA
+TAG=2.0-BETA
 OUTDIR="EPICSv4-Java-$TAG"
 TARFILE="$OUTDIR.tar.gz"
 mkdir -p $OUTDIR
@@ -40,7 +43,7 @@ cd $OUTDIR
 wget http://epics.sourceforge.net/maven2/epics/pvData/$TAG/pvData-$TAG.jar
 wget http://epics.sourceforge.net/maven2/epics/pvAccess/$TAG/pvAccess-$TAG.jar
 wget http://epics.sourceforge.net/maven2/epics/pvIOC/$TAG/pvIOC-$TAG.jar
-wget http://epics.sourceforge.net/maven2/epics/pvService/$TAG/pvService-$TAG.jar
+
 cd ..
 # For use of Java from jars, one still needs some source. xmls, example source, and common
 # setup scripts in particular.
@@ -50,8 +53,9 @@ hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata
 hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvServiceJava
 # For source of examples:
 hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/exampleJava
-# For common_setup.bash script:
-hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/common
+## For common_setup.bash script:
+# hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/common
+
 # For README. Note, no tag is used for pvDataWWW. We assume the README and other web files
 # may change following a tagged release, even though they refer to a tagged release.
 hg clone ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataWWW
@@ -62,9 +66,7 @@ hg clone ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataW
 # And server/!!! too.
 (cd pvIOCJava && hg archive -I "server/" ../$OUTDIR/pvIOCJava)
 
-(cd pvServiceJava && hg archive -I "xml" ../$OUTDIR/pvServiceJava)
 (cd exampleJava && hg archive ../$OUTDIR/exampleJava)
-(cd common && hg archive ../$OUTDIR/common)
 
 # There's a benign bug here: as code this creates a TAG/ dir at the same level as parent dir.
 # but it packages the tar correctly with just the README from pvDataWWW. 
@@ -74,4 +76,5 @@ rm -fr $OUTDIR/pvDataWWW
 
 echo $TAG > $OUTDIR/tag.txt
 tar czf $TARFILE $OUTDIR
+
 # rsync -a * $USER,epics-pvdata@frs.sourceforge.net:/home/frs/project/e/ep/epics-pvdata/$TAG
