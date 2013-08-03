93b78561474727eb4d5396abd91615079439b438
Unknown <greg@slac.stanford.edu>
2013-08-02 17:28:32 -0700

    Add full file-driven building release tar file

--- scripts/makereleaseJars.sh
+++ scripts/makereleaseJars.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 #!-*- sh -*-
 # 
-# Abs: makereleaseJars is a bash source script to manage the release of jar files and 
+# Summary:     
+#      ./makereleaseJars.sh -n <releaseName> [-l <hgrepodirname>] [-r <SFusername>]  
+#
+# Abstract: 
+#      makereleaseJars is a bash source script to manage the release of jar files and 
 #      associated deliverables for a version tagged release of the Java implementation 
 #      of EPICS v4. 
 #
 # Usage: 
-#      Following a tagged release of ALL 3 of the pv*Java modules (pvDataJava, pvIOCJava, pvAccessJava),
-#      as described in release.html, checkout makereleaseJars.sh, edit it to change the 
-#      USER to your sourceforge username, and TAG to the release tag, eg "1.0-BETA", then
-#      run it by hand from your own computer (as opposed to inside Jenkins) in some area
-#      that is not under hg (otherwise you'll have a clone inside the pvDataWWWW/scripts clone).
+#      Following a tagged release of ALL of the V4 core pv*Java modules (pvDataJava, pvAccessJava etc),
+#      as described in release.html, hg checkout pvDataWWW. Edit pvDataWWW/configure/RELEASE_VERSIONS 
+#      to define the modules of the release and their Mercurial (hg) tags. Note that 
+#      ** formally, the release is defined by specification of a set of module names and their 
+#      hg tag values, although for Java, we assume that there will be maven products for those
+#      tags at the time this script is run. This script actually packages the jar files in 
+#      the maven repo, it does not package from the hg repo directly!
 #
-#            cd ~
-#            mkdir tmp; cd tmp
-#            ./<pathto-pcDataWWW/scripts>/makereleaseJars.sh
-#  
+# Examples:
+#
 #      A user should unpack the resulting tar.gz with, for instance:
 #            tar zxvf EPICSv4-Java-1.0-BETA.tar.gz
 #
@@ -34,43 +38,125 @@
 #       and also include the common dir in the tar.gz. 
 
 # ============================================================================
-USER=gregorywhite
-TAG=2.0-BETA
-OUTDIR="EPICSv4-Java-$TAG"
-TARFILE="$OUTDIR.tar.gz"
-mkdir -p $OUTDIR
-cd $OUTDIR
-wget http://epics.sourceforge.net/maven2/epics/pvData/$TAG/pvData-$TAG.jar
-wget http://epics.sourceforge.net/maven2/epics/pvAccess/$TAG/pvAccess-$TAG.jar
-wget http://epics.sourceforge.net/maven2/epics/pvIOC/$TAG/pvIOC-$TAG.jar
+
+function usage { 
+echo "
+   makereleaseJars.sh creates the tar file of the Java modules collection of 
+   an EPICS V4 release. 
+
+   Usage:
+       ./makereleaseJars.sh -n <releaseName> [-l <hgrepodirname>] [-r <SFusername>]
+
+   -n <releaseName>  The string identifying the release. This is the key used to search
+                     RELEASE_VERSIONS to find the modules in the release
+
+   -l <hgrepodirname>  Use local files. A local RELEASE_VERSIONS file will be sought in
+                     ./<hgrepodirname>/pvDataWWW/scripts/RELEASE_VERSIONS, and the local 
+                     maven repository will be sought in ~/.m2/repository/epics/. If -l is 
+                     not given, the SourceForge hosted files will be used. 
+
+   -r <SFusername>   Use remote files. The argument SFusername is the SourceForge 
+                     username to be used for upload. 
+
+   Note that use of -l and -r are mutually exclusive.  
+"
+}
+
+declare -a modulesa
+
+# Remote location of the file which defines the versions of each package going into
+# tar file for the given release.
+RELEASE_VERSIONS_URL=\
+http://sourceforge.net/p/epics-pvdata/pvDataWWW/ci/default/tree/scripts/RELEASE_VERSIONS
+
+SFusername=
+releaseName=
+localfiles=0
+uploadtar=0
+if [ $# -lt 1 ]; then
+   echo "Not enough arguments, at least the -n releaseName must be given. " \
+        "See makereleaseJars.sh -h for help";
+   exit 1;
+fi
+while getopts hr:l:n: opt; do
+   case "$opt" in
+       h) usage; exit 0 ;;
+       r) uploadtar=1 
+          SFusername=${OPTARG} ;;
+       l) localfiles=1
+          hgrepodirname=${OPTARG} ;;
+       n) releaseName=${OPTARG} ;; 
+       *) echo "Unknown Argument, see makereleaseJars.sh -h"; exit 1;;
+   esac
+done
+shift $((OPTIND-1));
+if [ -z ${releaseName} ]; then
+    echo "The release name is a required argument, see makereleaseJars.sh -h"
+    exit 1
+fi
+
+outdir=${releaseName}
+tarfile="${releaseName}.tar.gz"
+
+# Locate the RELEASE_VERSIONS file, to tell use which modules must be in the release
+# build, and from it find out which modules are in the release. Check we got at 
+# least 1 module.
+#
+if [ ${localfiles} -eq 1 ]; then
+    release_versions_pathname=${hgrepodirname}/pvDataWWW/scripts/RELEASE_VERSIONS
+else
+    wget RELEASE_VERSIONS_URL
+    release_versions_pathname=${PWD}/RELEASE_VERSIONS 
+fi
+if [ ! -f ${release_versions_pathname} ]; then
+    echo "Failed to locate or use the RELEASE_VERSIONS file."
+    exit 2
+fi
+modulesa=(`awk -v relname=${releaseName} '$1 ~ relname {print $2}' < $release_versions_pathname`)
+if [ ${#modulesa[@]} -lt 1 ]; then
+    echo "Failed to find modules for release ${releaseName}"
+    exit 2
+fi
+
+# Create the directory whose contents we'll tar, and populate it. 
+#
+mkdir -p ${outdir}
+cd ${outdir}
+release_versions_pathname=../${release_versions_pathname}
+for modulei in $modulesa
+do
+    tag=`awk -v relname=${releaseName} -v modulename=${modulei} \
+          '$1 ~ relname && $2 ~ modulename {print $3}' < $release_versions_pathname`
+
+    if [ $? -eq 0 ]; then
+
+       # If no SourceForge user name was given, assume we're packaging from files 
+    # in a local maven repository, and further assume it's in ~./m2/repository/
+    #
+    if [ ${localfiles} -eq 1 ]; then
+	cp ~/.m2/repository/epics/${modulei}/${tag}/${modulei}-${tag}.jar .
+	cp ~/.m2/repository/epics/${modulei}/${tag}/${modulei}-${tag}.pom .
+	cp ~/.m2/repository/epics/${modulei}/${tag}/${modulei}-${tag}-sources.jar .
+	cp ~/.m2/repository/epics/${modulei}/${tag}/${modulei}-${tag}-javadoc.jar .
+    else
+	wget http://epics.sourceforge.net/maven2/epics/${modulei}/${tag}/${modulei}-${tag}.jar
+	wget http://epics.sourceforge.net/maven2/epics/${modulei}/${tag}/${modulei}-${tag}.pom
+	wget http://epics.sourceforge.net/maven2/epics/${modulei}/${tag}/${modulei}-${tag}-sources.jar
+	wget http://epics.sourceforge.net/maven2/epics/${modulei}/${tag}/${modulei}-${tag}-javadoc.jar
+        wget http://epics-pvdata.sourceforge.net/README
+    fi
+    else
+	echo "Could not get module version for ${modulei}, exiting"
+	exit 3
+    fi
+done
 
 cd ..
-# For use of Java from jars, one still needs some source. xmls, example source, and common
-# setup scripts in particular.
-# For XMLs:
-hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvIOCJava
-# For source of examples:
-hg clone -r $TAG ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/exampleJava
 
-# For README. Note, no tag is used for pvDataWWW. We assume the README and other web files
-# may change following a tagged release, even though they refer to a tagged release.
-hg clone ssh://$USER@epics-pvdata.hg.sourceforge.net/hgroot/epics-pvdata/pvDataWWW
+tar czf $tarfile $outdir
 
-(cd pvIOCJava && hg archive -I "xml/" ../$OUTDIR/pvIOCJava)
-# Hm, need src/ too for the runtime!!!! Because it has XMLs in it. Argghhhhhhhhhhhhhhhhh
-(cd pvIOCJava && hg archive -I "src/" ../$OUTDIR/pvIOCJava)
-# And server/!!! too.
-(cd pvIOCJava && hg archive -I "server/" ../$OUTDIR/pvIOCJava)
+if [ ${uploadtar} -eq 1 ]; then
+   rsync -a * $SFusername,epics-pvdata@frs.sourceforge.net:/home/frs/project/e/ep/epics-pvdata/$tag
+fi
 
-(cd exampleJava && hg archive ../$OUTDIR/exampleJava)
-
-# There's a benign bug here: as code this creates a TAG/ dir at the same level as parent dir.
-# but it packages the tar correctly with just the README from pvDataWWW. 
-(cd pvDataWWW && hg archive -I mainPage/README ../../$OUTDIR)
-cp pvDataWWW/mainPage/README $OUTDIR
-rm -fr $OUTDIR/pvDataWWW
-
-echo $TAG > $OUTDIR/tag.txt
-tar czf $TARFILE $OUTDIR
-
-# rsync -a * $USER,epics-pvdata@frs.sourceforge.net:/home/frs/project/e/ep/epics-pvdata/$TAG
+exit 0
