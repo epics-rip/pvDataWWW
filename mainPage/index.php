d238235b04732fe43fe1f7cec69d3d8a3bcdd232
Ralph Lange <ralph.lange@gmx.de>
2020-03-09 19:25:05 +0100

    Add PHP redirect and note on top of main page

---
+++ mainPage/index.php
@@ -0,0 +1,6 @@
+<?php
+/* Redirect browser */
+header("Location: https://epics-controls.org/resources-and-support/base/epics-7");
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
